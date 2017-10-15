19728b2d49d67f55dea27c0edc4b6ceb876d89ea
Dima Golomozy <dima.golomozy@gmail.com>
2017-10-16 01:52:16 +0300

    first dummy commit

---
+++ src/main/java/com/dg/rabbitmq/concurrent/Semaphore.java
@@ -0,0 +1,8 @@
+package com.dg.rabbitmq.concurrent;
+
+/**
+ * @author dima.golomozy
+ */
+public class Semaphore {
+
+}
---
+++ src/main/java/com/dg/rabbitmq/internal/Internal.java
@@ -0,0 +1,7 @@
+package com.dg.rabbitmq.internal;
+
+/**
+ * @author dima.golomozy
+ */
+public class Internal {
+}
